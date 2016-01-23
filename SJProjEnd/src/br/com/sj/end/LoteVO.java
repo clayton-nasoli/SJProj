3674600286aea65e33d317477791da4668e75aad
clayton <clayton.nasoli@gmail.com>
2016-01-23 17:25:17 -0200

    Teste de novo Commit (mudanças).

--- SJProjEnd/src/br/com/sj/end/LoteVO.java
+++ SJProjEnd/src/br/com/sj/end/LoteVO.java
@@ -3,7 +3,14 @@ package br.com.sj.end;
 public class LoteVO {
 	private String numero;
 	private String destinacao;
+	private boolean isCondominio;
 	
+	public boolean isCondominio() {
+		return isCondominio;
+	}
+	public void setCondominio(boolean isCondominio) {
+		this.isCondominio = isCondominio;
+	}
 	public String getNumero() {
 		return numero;
 	}
