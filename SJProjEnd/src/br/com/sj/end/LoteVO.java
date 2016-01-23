eb831a363b03c33cbf4e881391a29d4c1ca72b5d
clayton <clayton.nasoli@gmail.com>
2016-01-23 17:12:04 -0200

    Teste de primeiro commit.

---
+++ SJProjEnd/src/br/com/sj/end/LoteVO.java
@@ -0,0 +1,19 @@
+package br.com.sj.end;
+
+public class LoteVO {
+	private String numero;
+	private String destinacao;
+	
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getDestinacao() {
+		return destinacao;
+	}
+	public void setDestinacao(String destinacao) {
+		this.destinacao = destinacao;
+	}
+}
